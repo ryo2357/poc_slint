15c0b0c1a8b9caa3696a7124320077d0251d7b67
ryo2357 <miyawakir@hosokawa-yoko.co.jp>
2023-12-26 10:40:11 +0900

    clickの検証

---
+++ examples/click.rs
@@ -0,0 +1,45 @@
+// [Polishing the Tile - Slint Memory Game Tutorial (Rust)](https://slint.dev/releases/1.3.2/docs/tutorial/rust/polishing_the_tile)
+slint::slint! {
+
+    export component MainWindow inherits Window {
+        // width: 326px;
+        // height: 326px;
+        out property<length> mouse_x;
+        out property<length> mouse_y;
+        callback clicked;
+        in property window_width <=> self.width;
+        in property window_height <=> self.height;
+
+        TouchArea {
+            clicked => {
+                root.mouse_x = self.pressed-x;
+                root.mouse_y = self.pressed-y;
+                root.clicked();
+
+            }
+        }
+    }
+
+}
+
+fn main() -> anyhow::Result<()> {
+    let mut ui = MainWindow::new()?;
+    ui.set_window_width(500.0);
+    ui.set_window_height(400.0);
+
+    let weak = ui.as_weak();
+
+    ui.on_clicked(move || {
+        let app = weak.upgrade().unwrap();
+        println!("Clicked");
+        let x = app.get_mouse_x();
+        let y = app.get_mouse_y();
+
+        println!("x:{:?},y:{:?}", x, y);
+        app.set_window_width(800.0);
+        app.set_window_height(800.0);
+    });
+
+    ui.run()?;
+    Ok(())
+}
